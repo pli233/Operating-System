74482734b56f138069b2bfd1a8743c7b96b2f93b
pli233 <peiyuanli3627@gmail.com>
2023-09-08 14:36:28 -0500

    finish wman

--- p1-main/wman.c
+++ p1-main/wman.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <ctype.h>
 
 int read_and_print(char* file_path){
 
@@ -10,7 +11,7 @@ int read_and_print(char* file_path){
 
     // Check if the file was successfully opened
     if (file == NULL) {
-        printf("Error opening the file.\n");
+        printf("cannot open file\n");
         return 1; // Exit with an error code
     }
 
@@ -33,8 +34,7 @@ int read_and_print(char* file_path){
 /**
  * This function will return the 
 */
-
-void search_files(const char* path, const char* target_name) {
+int strict_search_files(const char* path, const char* target_name) {
     DIR *dir;
     struct dirent *entry;
 
@@ -43,57 +43,78 @@ void search_files(const char* path, const char* target_name) {
 
     if (dir == NULL) {
         perror("Error opening directory");
-        return;
+        return 1;
     }
 
     // Read and print file and directory names
     while ((entry = readdir(dir))) {
+        //printf("%s\n", entry->d_name);
+        //Case1: Reach out a Regular File 
         if (entry->d_type == DT_REG && strcmp(entry->d_name, target_name) == 0) {
             char full_path[256];
             snprintf(full_path, sizeof(full_path), "%s/%s", path, target_name);
-            read_and_print(full_path);
-            return; // Exit after finding the file
-        }
-        if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-            char full_path[256];
-            snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
-            search_files(full_path, target_name); // Recursively search directories
+            closedir(dir);
+            return read_and_print(full_path);; // Exit after finding the file
         }
     }
     closedir(dir);
+    return 1;
 }
-
-int main() {
-    const char* folderPath = "/path/to/your/folder";
-    const char* targetFileName = "your_target_file.txt";
-    search_files(folderPath, targetFileName);
-    return 0;
-}
-
-
-
 
 int main(int argc, char *argv[]) {
-    printf("Number of arguments: %d\n", argc-1);
-
+    //printf("Number of arguments: %d\n", argc-1);
 
     // Base Case: Check if there are any command-line arguments
-    if (argc <2) {
-        printf("No command-line arguments provided.\n");
+    if (argc < 2) {
+        printf("What manual page do you want?\nFor example, try 'wman wman'\n");
         return 1; // Exit with an error code
     }
-    // Case1: Handle one argument execution
-    else if (argc-1 == 1)
+    // Case1: Handle one argument execution, search the command name in each subfile
+    else if (argc == 2)
     {
-        char* filename = argv[1];
-    }
-    // Case2: Handle two arguments execution
-    else if (argc-1 == 2){
-
-    }
-    // Case3: Check if there are more than two arguments
-    else{
-         printf("Too many arguments provided.\n");
+        const char* root_dir = "./man_pages/man";
+        char* page = argv[1];
+        char filepath[256];
+        char filename[256];
+        for(int section = 1; section <= 9; section++) {
+            snprintf(filepath, sizeof(filepath), "%s%d", root_dir, section);
+            snprintf(filename, sizeof(filename), "%s.%d", page, section);
+            // printf("%s\n",filepath);
+            // printf("%s\n",filename);
+            if(strict_search_files(filepath,filename) == 0){
+                return 0;
+            }
+        }
+        printf("No manual entry for %s\n", page);    
         return 1;
     }
+    // Case2: Handle two arguments execution
+    else if (argc == 3){
+        const char* root_dir = "./man_pages/man";
+        char* page = argv[2];
+        char* section= argv[1];
+
+        if(strlen(section) != 1 || section[0] < '0' || section[0] > '9'){
+            printf("invalid section\n");
+            return 1;
+        }
+        char filepath[256];
+        char filename[256];
+
+        snprintf(filepath, sizeof(filepath), "%s%s", root_dir, section);
+        snprintf(filename, sizeof(filename), "%s.%s", page, section);
+
+        if(strict_search_files(filepath,filename) == 0){
+            return 0;
+        }
+        printf("No manual entry for %s in section %s\n", page, section);
+        return 1;
+    }
+    // Case3: Check if there are more than two arguments
+    else {
+        printf("What manual page do you want?\nFor example, try 'wman wman'\n");
+        return 1;
+    }
+
+    return 0;
 }
